17065d75d8679ab9ff06301b49618de7d11967fe
용규 <105268330+dydrb@users.noreply.github.com>
2023-08-25 09:49:33 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,14 @@
+x = input()
+y = input()
+
+x = int(x)
+y = int(y)
+
+if x > 0 and y > 0:
+    print(1)
+elif x > 0 and y < 0:
+    print(4)
+elif x < 0 and y > 0:
+    print(2)
+else:
+    print(3)
