4327dfe5e4862e4ce132900a61d34528c43a3853
용규 <105268330+dydrb@users.noreply.github.com>
2023-10-11 19:07:37 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+X = int(input())
+
+if 100 >= X and X >= 90:
+    print('A')
+elif 89 >= X and X >= 80:
+    print('B')
+elif 79 >= X and X >= 70:
+    print('C')
+elif 69 >= X and X >= 60:
+    print('D')
+else:
+    print("F")
