ac02744e046172015ca37984fcaa99731f44dfb9
용규 <105268330+dydrb@users.noreply.github.com>
2023-10-13 10:53:22 +0900

    [Bronze III] Title: ACM 호텔, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -0,0 +1,12 @@
+in_num = int(input())
+
+for i in range(in_num):
+    H, W, N = map(int, input().split())
+    floor = N % H
+    room = N // H + 1
+
+    if floor == 0:
+        room -= 1
+        floor = H
+ 
+    print(floor*100 + room)
